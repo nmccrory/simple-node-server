abe3eedd8f8fa78f7a0e3a83402e1b0fa1264b6f
Nick McCrory <n.mccrory@icloud.com>
2015-06-17 16:53:32 -0700

    first commit

---
+++ simpleserver.js
@@ -0,0 +1,26 @@
+//get the http module:
+var http = require('http');
+//fs module reads and writes content for responses
+var fs = require('fs');
+//create server with http module:
+var server = http.createServer(function(request,response){
+	//read url
+	console.log('client request URL: ', request.url);
+	//routing
+	if(request.url === '/'){
+		fs.readFile('index.html', 'utf8', function(errors,contents){
+			response.writeHead(200,{'Content-Type': 'text/html'}); //info about response
+			response.write(contents);
+			response.end();
+		});
+	}
+
+	//if request has no match
+	else{
+		response.writeHead(404);
+		response.end('File not found!');
+	}
+})
+//specifying port
+server.listen(6789);
+console.log("running in localhost port 6789");
